ef9b3065afad0c4929662fddf1d3d8f0498a26e0
Szymon <kosinski.szymon@o2.pl>
2025-02-12 21:27:32 +0100

    Fix: added missing files to service directory

---
+++ reports-service/src/main/java/io/swagger/configuration/SwaggerDocumentationConfig.java
@@ -0,0 +1,29 @@
+package io.swagger.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+
+@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2024-10-23T20:13:09.961561888Z[GMT]")
+@Configuration
+public class SwaggerDocumentationConfig {
+
+    @Bean
+    public OpenAPI openApi() {
+        return new OpenAPI()
+            .info(new Info()
+                .title("Events and Raports API")
+                .description("API to manage events and raports")
+                .termsOfService("")
+                .version("1.0.0")
+                .license(new License()
+                    .name("")
+                    .url("http://unlicense.org"))
+                .contact(new io.swagger.v3.oas.models.info.Contact()
+                    .email("")));
+    }
+
+}
